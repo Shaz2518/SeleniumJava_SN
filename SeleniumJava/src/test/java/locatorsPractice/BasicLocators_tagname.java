b207c846473d3f805ac708c5d5a3a354813317e4
ShaziyaShaikh <shaziyashaikh2518@outlook.com>
2025-12-17 21:14:51 +0530

    Locators- tagname

---
+++ SeleniumJava/src/test/java/locatorsPractice/BasicLocators_tagname.java
@@ -0,0 +1,28 @@
+package locatorsPractice;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BasicLocators_tagname {
+
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.saucedemo.com/");
+		
+		List<WebElement> list = driver.findElements(By.tagName("div"));
+		for(WebElement x : list)
+		{
+			String divValue= x.getText();
+			System.out.println(divValue);
+			
+		}
+		driver.close();
+
+	}
+
+}
