64e0c9d0cbad92d2518b81ad53b3aa6780857bed
ShaziyaShaikh <shaziyashaikh2518@outlook.com>
2025-12-18 21:03:16 +0530

    Partal Link Text

--- SeleniumJava/src/test/java/locatorsPractice/BasicLocator_LinkText.java
+++ SeleniumJava/src/test/java/locatorsPractice/BasicLocator_LinkText.java
@@ -1,18 +1,36 @@
 package locatorsPractice;
 
+import java.util.List;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
 
 public class BasicLocator_LinkText {
 
 	public static void main(String[] args) throws InterruptedException {
 		
-		WebDriver driver = new ChromeDriver();
-		driver.get("https://demo.opencart.com/");
-		Thread.sleep(50000);
-		driver.findElement(By.linkText("Cameras")).click();
+		WebDriver driver = new EdgeDriver();
+		driver.get("https://www.amazon.in");
+		Thread.sleep(2000);
+		
+//		List<WebElement> element = driver.findElements(By.className("navFooterVerticalRow"));
+//		for(WebElement e : element)
+//			{
+//			System.out.println(e.getText());
+//			}
+//		
+		//Through tag name
+//		List<WebElement> tagElements = driver.findElements(By.tagName("div"));
+//		for(WebElement x : tagElements)
+//		{
+//			System.out.println(x.getText());
+//		}
+		
+		//Link Text
+		driver.findElement(By.linkText("Today's Deals")).click();
 
 	}
-
 }
+
+
