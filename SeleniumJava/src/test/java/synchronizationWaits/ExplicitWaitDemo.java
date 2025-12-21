bb5a02b7b55fbb4598604b5a66bd54776b81c2ae
ShaziyaShaikh <shaziyashaikh2518@outlook.com>
2025-12-21 19:07:32 +0530

    Explicit Wait

---
+++ SeleniumJava/src/test/java/synchronizationWaits/ExplicitWaitDemo.java
@@ -0,0 +1,29 @@
+package synchronizationWaits;
+
+import java.time.Duration;
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class ExplicitWaitDemo {
+
+	public static void main(String[] args) {
+		WebDriver driver = new EdgeDriver();
+		driver.manage().window().maximize();
+		
+		//Explicit Wait
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+		driver.get("https://opensource-demo.orangehrmlive.com/");
+
+		WebElement userText = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("username")));
+		userText.sendKeys("Alpha");
+		
+
+	}
+
+}
