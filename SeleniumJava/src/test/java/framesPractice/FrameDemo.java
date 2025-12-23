583c13a57d5b14cd08e1073c6c52ea63397b1cca
ShaziyaShaikh <shaziyashaikh2518@outlook.com>
2025-12-23 20:32:04 +0530

    Frame Handling

--- SeleniumJava/src/test/java/framesPractice/FrameDemo.java
+++ SeleniumJava/src/test/java/framesPractice/FrameDemo.java
@@ -1,5 +1,7 @@
 package framesPractice;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -16,9 +18,7 @@ public class FrameDemo {
 		//Switch to Frame1 and Enter text
 		WebElement frame1 = driver.findElement(By.xpath("//frame[@src='frame_1.html']"));
 		driver.switchTo().frame(frame1);
-		
-		WebElement textBox = driver.findElement(By.name("mytext1"));
-		textBox.sendKeys("Hello");
+		driver.findElement(By.name("mytext1")).sendKeys("Hello");
 		
 		//Switch to Main WebPage
 		driver.switchTo().defaultContent();
@@ -26,10 +26,28 @@ public class FrameDemo {
 		//Switch to Frame 2
 		WebElement frame2 = driver.findElement(By.xpath("//frame[@src='frame_2.html']"));
 		driver.switchTo().frame(frame2);
+		driver.findElement(By.name("mytext2")).sendKeys("World");
 		
-		WebElement textBox1 = driver.findElement(By.name("mytext2"));
-		textBox1.sendKeys("World");
-
+		//Switch to Main WebPage
+		driver.switchTo().defaultContent();
+		
+		//Switch to frame 3
+		WebElement frame3 = driver.findElement(By.xpath("//frame[@src='frame_3.html']"));
+		driver.switchTo().frame(frame3);
+		WebElement text = driver.findElement(By.name("mytext3"));
+		text.sendKeys("Java Practice");
+		//Switch to nested frame in Frame3
+		WebElement iframeElement = driver.findElement(By.tagName("iframe"));
+		driver.switchTo().frame(iframeElement);
+		
+		driver.findElement(By.xpath("//span[text()='I am a human']")).click();
+		driver.findElement(By.xpath("//span[text()='Web Testing']")).click();
+		
+		driver.switchTo().parentFrame();
+		
+		//driver.switchTo().frame(frame3);
+		text.clear();
+		text.sendKeys("Selenium");
 	}
 
 }
