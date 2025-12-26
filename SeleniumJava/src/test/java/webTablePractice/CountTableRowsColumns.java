d5accfbbb7114ba93a4248186246fd2c7d48360e
ShaziyaShaikh <shaziyashaikh2518@outlook.com>
2025-12-26 23:37:10 +0530

    Web Table

--- SeleniumJava/src/test/java/webTablePractice/CountTableRowsColumns.java
+++ SeleniumJava/src/test/java/webTablePractice/CountTableRowsColumns.java
@@ -7,24 +7,24 @@ import org.openqa.selenium.edge.EdgeDriver;
 public class CountTableRowsColumns {
 
 	public static void main(String[] args) {
-		
+
 		WebDriver driver = new EdgeDriver();
 		driver.manage().window().maximize();
-		
+
 		driver.get("https://testautomationpractice.blogspot.com/");
-		
-		//Count number of rows in table
+
+		// Count number of rows in table
 		int tableRow = driver.findElements(By.xpath("//table[@name='BookTable']//tr")).size();
 		System.out.println("Table Row: " + tableRow);
-		
-		//Count number of columns
+
+		// Count number of columns
 		int tableCol = driver.findElements(By.xpath("//table[@name='BookTable']//th")).size();
 		System.out.println("Table Columns: " + tableCol);
-		
-		//Retrieve specific data
+
+		// Retrieve specific data
 		String value1 = driver.findElement(By.xpath("//table[@name='BookTable']//tr[4]/td[3]")).getText();
 		System.out.println("TR-4 TD-3: " + value1);
-		
+
 		driver.quit();
 	}
 
